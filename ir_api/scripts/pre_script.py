eba2bbfa2ddebe0e1e5c1906b8911d3ccf9ab1dc
Keiran Price <keiran.price@stfc.ac.uk>
2023-04-17 08:25:20 +0100

    Create PreScript

---
+++ ir_api/scripts/pre_script.py
@@ -0,0 +1,34 @@
+"""
+PreScript contains the PreScript definition. As it is not strictly a model, it does not belong in models.py
+"""
+from ir_api.core.responses import PreScriptResponse
+
+
+class PreScript:
+    """
+    The PreScript is the script obtained from GitHub, prior to being used by the JobController, or being saved to the
+    database.
+    """
+
+    def __init__(self, value: str, is_latest: bool = False):
+        self.value = value
+        self._original_value = value
+        self.is_latest = is_latest
+
+    @property
+    def original_value(self) -> str:
+        """
+        Read only original_value is the value before any transforms are applied.
+        :return: str - original value  pre transforms
+        """
+        return self._original_value
+
+    def to_response(self) -> PreScriptResponse:
+        """
+        Return a ScriptResponse model to be returned by the api
+        :return: ScriptResponse - the ScriptResponse
+        """
+        return PreScriptResponse(value=self.value, is_latest=self.is_latest)
+
+    def __repr__(self) -> str:
+        return f"PreScript(value={self.value}, is_latest={self.is_latest})"
