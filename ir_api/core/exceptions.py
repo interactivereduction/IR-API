d219852f65ce6466c2fc4c1aa9086b0bf642cc57
Keiran Price <keiran.price@stfc.ac.uk>
2023-04-17 08:22:01 +0100

    Add custom exceptions

---
+++ ir_api/core/exceptions.py
@@ -0,0 +1,27 @@
+"""
+Custom Exceptions
+"""
+
+
+class DatabaseError(Exception):
+    """
+    Database specific error
+    """
+
+
+class MissingRecordError(DatabaseError):
+    """
+    Record was requested but did not exist
+    """
+
+
+class NonUniqueRecordError(DatabaseError):
+    """
+    Multiple records were found when only a single was expected
+    """
+
+
+class MissingScriptError(Exception):
+    """
+    No script could be found on remote or on github, it is likely the instrument does not exist
+    """
