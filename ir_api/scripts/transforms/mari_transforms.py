1d25eb7e74afadf20f7aa2656134d37ba5d8eb2e
Keiran Price <keiran.price@stfc.ac.uk>
2023-04-17 08:28:42 +0100

    update mari transform

--- ir_api/scripts/transforms/mari_transforms.py
+++ ir_api/scripts/transforms/mari_transforms.py
@@ -1,28 +1,47 @@
-from ir_api.core.entities import Script, Reduction
+"""
+Module provides the MariTransform class, an implementation of the Transform abstract base class for MARI instrument
+scripts.
+"""
+from typing import List
+
+from sqlalchemy import ColumnElement
+from sqlalchemy.dialects.postgresql import JSONB
+
+from ir_api.core.model import Reduction
+from ir_api.scripts.pre_script import PreScript
 from ir_api.scripts.transforms.transform import Transform
 
 
 class MariTransform(Transform):
-    def apply(self, script: Script, reduction: Reduction) -> None:
+    """
+    MariTransform applies modifications to MARI instrument scripts based on reduction input parameters in a Reduction
+    entity.
+    """
+
+    def apply(self, script: PreScript, reduction: Reduction) -> None:
         lines = script.value.splitlines()
+        # MyPY does not believe ColumnElement[JSONB] is indexable, despite JSONB implementing the Indexable mixin
         for index, line in enumerate(lines):
-            if self._replace_line(line, lines, index, "runno", reduction.reduction_inputs["runno"]):
+            if self._replace_input(line, lines, index, "runno", reduction.reduction_inputs["runno"]):  # type: ignore
                 continue
-            if self._replace_line(line, lines, index, "sum_runs", reduction.reduction_inputs["sum_runs"]):
+            if self._replace_input(line, lines, index, "sum_runs", reduction.reduction_inputs["sum_runs"]):  # type: ignore
                 continue
-            if self._replace_line(line, lines, index, "ei", reduction.reduction_inputs["ei"]):
+            if self._replace_input(line, lines, index, "ei", reduction.reduction_inputs["ei"]):  # type: ignore
                 continue
-            if self._replace_line(line, lines, index, "monovan", reduction.reduction_inputs["monovan"]):
+            if self._replace_input(line, lines, index, "monovan", reduction.reduction_inputs["monovan"]):  # type: ignore
                 continue
-            if self._replace_line(line, lines, index, "sam_mass", reduction.reduction_inputs["sam_mass"]):
+            if self._replace_input(line, lines, index, "sam_mass", reduction.reduction_inputs["sam_mass"]):  # type: ignore
                 continue
-            if self._replace_line(line, lines, index, "sam_rmm", reduction.reduction_inputs["sam_rmm"]):
+            if self._replace_input(line, lines, index, "sam_rmm", reduction.reduction_inputs["sam_rmm"]):  # type: ignore
                 continue
-            if self._replace_line(line, lines, index, "remove_bkg", reduction.reduction_inputs["remove_bkg"]):
+            if self._replace_input(line, lines, index, "remove_bkg", reduction.reduction_inputs["remove_bkg"]):  # type: ignore
                 continue
         script.value = "\n".join(lines)
 
-    def _replace_line(self, line, lines, index, line_start, replacement):
+    @staticmethod
+    def _replace_input(
+        line: str, lines: List[str], index: int, line_start: str, replacement: ColumnElement["JSONB"]
+    ) -> bool:
         if line.startswith(line_start):
             lines[index] = f"{line_start} = {replacement}"
             return True
