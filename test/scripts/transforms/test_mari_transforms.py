58dcf5725b5a7a20c04a5eba216848139305b185
Keiran Price <keiran.price@stfc.ac.uk>
2023-05-02 15:47:07 +0100

    Update script in test

--- test/scripts/transforms/test_mari_transforms.py
+++ test/scripts/transforms/test_mari_transforms.py
@@ -27,11 +27,11 @@ with open("MARIReduction_Sample.py", "w+") as fle:
     text = requests.get("https://raw.githubusercontent.com/mantidproject/scriptrepository/master/direct_inelastic/MARI/MARIReduction_Sample.py").text
     fle.write(text)
     
-with open("mask_file.xml", "w+" as fle:
+with open("mask_file.xml", "w+" as fle):
     text = requests.get("url_to_mask_file.xml").text
     fle.write(text)
     
-with open("mari_res2013.map", "w+" as fle:
+with open("mari_res2013.map", "w+" as fle):
     text = requests.get("https://raw.githubusercontent.com/pace-neutrons/InstrumentFiles/964733aec28b00b13f32fb61afa363a74dd62130/mari/mari_res2013.map").text
     fle.write(text)
 
