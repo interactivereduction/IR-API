b1ef092a10db361a9d59b04f3307ffb87d4f2122
Keiran Price <keiran.price@stfc.ac.uk>
2023-07-06 00:35:21 +0100

    Add test transform

--- ir_api/scripts/transforms/factory.py
+++ ir_api/scripts/transforms/factory.py
@@ -2,6 +2,7 @@
 This module provides a factory function to get the appropriate transform for a given instrument.
 """
 from ir_api.scripts.transforms.mari_transforms import MariTransform
+from ir_api.scripts.transforms.test_transforms import TestTransform
 from ir_api.scripts.transforms.transform import Transform, MissingTransformError
 
 
@@ -14,5 +15,7 @@ def get_transform_for_instrument(instrument: str) -> Transform:
     match instrument.lower():
         case "mari":
             return MariTransform()
+        case "test":
+            return TestTransform()
         case _:
             raise MissingTransformError(f"No transform for instrument {instrument}")
