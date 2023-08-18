23b02d3747f404e84187ad4d410ca6167a031f34
Keiran Price <keiran.price@stfc.ac.uk>
2023-08-18 09:25:08 +0100

    Test reduction service

--- ir_api/core/services/reduction.py
+++ ir_api/core/services/reduction.py
@@ -1,17 +1,16 @@
 """
 Service Layer for reductions
 """
-from typing import Optional, Sequence
+from typing import Sequence
 
+from ir_api.core.exceptions import MissingRecordError
 from ir_api.core.model import Reduction, Run, Instrument
 from ir_api.core.repositories import ReductionRepo
 
 _REDUCTION_REPO = ReductionRepo()
 
 
-def get_reductions_by_instrument(
-    instrument: str, limit: Optional[int] = None, offset: Optional[int] = None
-) -> Sequence[Reduction]:
+def get_reductions_by_instrument(instrument: str, limit: int = 0, offset: int = 0) -> Sequence[Reduction]:
     """
     Given an instrument name return a sequence of reductions for that instrument. Optionally providing a limit and
     offset to be applied to the sequence
@@ -24,4 +23,28 @@ def get_reductions_by_instrument(
         lambda reduction: reduction.runs.any(Run.instrument.has(Instrument.instrument_name == instrument)),
         limit=limit,
         offset=offset,
+    )
+
+
+def get_reduction_by_id(id: int) -> Reduction:
+    """
+    Given an ID return the reduction with that ID
+    :param id: The id of the reduction to search for
+    :return: The reduction
+    :raises: MissingRecordError when no reduction for that ID is found
+    """
+    reduction = _REDUCTION_REPO.find_one(lambda reduction: reduction.id == id)
+    if reduction is None:
+        raise MissingRecordError(f"No Reduction for id {id}")
+    return reduction
+
+
+def get_reductions_by_experiment_number(experiment_number: int, limit: int = 0, offset: int = 0) -> Sequence[Reduction]:
+    """
+    Given an experiment number, return all reductions for that experiment
+    :param experiment_number: The experiment number
+    :return: List of reductions
+    """
+    return _REDUCTION_REPO.find(
+        lambda reduction: reduction.runs.any(Run.experiment_number == experiment_number), limit=limit, offset=offset
     )
