08a4c60b595d8344b201837771a8059f0bae7882
Keiran Price <keiran.price@stfc.ac.uk>
2023-08-18 15:33:51 +0100

    Add routes update e2e tests

--- ir_api/router.py
+++ ir_api/router.py
@@ -1,12 +1,17 @@
 """
 Module containing the REST endpoints
 """
-from typing import Optional
+from typing import Optional, List
 
 from fastapi import APIRouter
 from starlette.background import BackgroundTasks
 
-from ir_api.core.responses import PreScriptResponse
+from ir_api.core.responses import PreScriptResponse, ReductionResponse, ReductionWithRunsResponse
+from ir_api.core.services.reduction import (
+    get_reductions_by_instrument,
+    get_reduction_by_id,
+    get_reductions_by_experiment_number,
+)
 from ir_api.scripts.acquisition import get_script_for_reduction, write_script_locally
 from ir_api.scripts.pre_script import PreScript
 
@@ -32,3 +37,43 @@ async def get_pre_script(
     finally:
         background_tasks.add_task(write_script_locally, script, instrument)
         # write the script after to not slow down request
+
+
+@ROUTER.get("/instrument/{instrument}/reductions")
+async def get_reductions_for_instrument(instrument: str, limit: int = 0, offset: int = 0) -> List[ReductionResponse]:
+    """
+    Retrieve a list of reductions for a given instrument.
+    :param instrument: the name of the instrument
+    :param limit: optional limit for the number of reductions returned (default is 0, which can be interpreted as no limit)
+    :param offset: optional offset for the list of reductions (default is 0)
+    :return: List of ReductionResponse objects
+    """
+    instrument = instrument.upper()
+    reductions = get_reductions_by_instrument(instrument, limit=limit, offset=offset)
+    return [ReductionResponse.from_reduction(r) for r in reductions]
+
+
+@ROUTER.get("/reduction/{id}")
+async def get_reduction(id: int) -> ReductionWithRunsResponse:
+    """
+    Retrieve a reduction with nested run data, by iD.
+    :param id: the unique identifier of the reduction
+    :return: ReductionWithRunsResponse object
+    """
+    reduction = get_reduction_by_id(id)
+    return ReductionWithRunsResponse.from_reduction(reduction)
+
+
+@ROUTER.get("/experiment/{experiment_number}/reductions")
+async def get_reductions_for_experiment(
+    experiment_number: int, limit: int = 0, offset: int = 0
+) -> List[ReductionResponse]:
+    """
+    Retrieve a list of reductions associated with a specific experiment number.
+    :param experiment_number: the unique experiment number
+    :return: List of ReductionResponse objects
+    """
+    return [
+        ReductionResponse.from_reduction(r)
+        for r in get_reductions_by_experiment_number(experiment_number, limit=limit, offset=offset)
+    ]
--- test/test_e2e.py
+++ test/test_e2e.py
@@ -112,6 +112,48 @@ def test_get_default_prescript_instrument_does_not_exist():
     }
 
 
+def test_get_reduction_by_id_reduction_doesnt_exist():
+    """
+    Test 404 for reduction not existing
+    :return:
+    """
+    response = client.get("/reduction/123144324234234234")
+    assert response.status_code == 404
+    assert response.json() == {"message": "Resource not found"}
+
+
+def test_get_reduction_by_id_reduction_exists():
+    """
+    Test reduction returned for id that exists
+    :return:
+    """
+    response = client.get("/reduction/1")
+    assert response.status_code == 200
+    assert response.json() == {
+        "id": 1,
+        "reduction_end": "2021-02-09T03:59:48",
+        "reduction_inputs": {"age": "WtlvblwzmmlxpbRCHjZS", "agree": "MUYAYAVjoYzWuSyMvItT", "rate": True},
+        "reduction_outputs": "What should this be?",
+        "reduction_start": "2021-02-09T03:31:48",
+        "reduction_state": "UNSUCCESSFUL",
+        "reduction_status_message": "Prepare small behind agreement structure item several able knowledge return director war.",
+        "runs": [
+            {
+                "experiment_number": 61544,
+                "filename": "/archive/NDXHET/Instrument/data/cycle_17_03/HET61544.nxs",
+                "good_frames": 4011,
+                "instrument_name": "HET",
+                "raw_frames": 7070,
+                "run_end": "2020-11-17T04:22:10",
+                "run_start": "2020-11-17T03:49:10",
+                "title": "Light final summer pass official positive.",
+                "users": "Jeremy Smith, James Mora",
+            }
+        ],
+        "script": {"value": "import os\nprint('foo')\n"},
+    }
+
+
 @patch("ir_api.scripts.acquisition.LOCAL_SCRIPT_DIR", "ir_api/local_scripts")
 def test_get_prescript_when_reduction_does_not_exist():
     """
@@ -151,3 +193,99 @@ def something() -> None:
 
 something()"""
     )
+
+
+def test_get_reductions_for_experiment_number():
+    """
+    Test reduction returned for experiment
+    :return:
+    """
+    response = client.get("/experiment/36803/reductions")
+    assert response.status_code == 200
+    assert response.json() == [
+        {
+            "id": 2133,
+            "reduction_end": "2020-10-04T07:45:05",
+            "reduction_inputs": {"figure": "YinqTBddoeJWMVahyATY", "focus": -73552971014.5794},
+            "reduction_outputs": "What should this be?",
+            "reduction_start": "2020-10-04T07:20:05",
+            "reduction_state": "SUCCESSFUL",
+            "reduction_status_message": "Open for add face receive force fly always nature.",
+            "script": {"value": "import os\nprint('foo')\n"},
+        }
+    ]
+
+
+def test_get_reductions_for_experiment_number_does_not_exist():
+    """
+    Test empty array returned when no reduction for an experiment number
+    :return:
+    """
+    response = client.get("/experiment/12345678/reductions")
+    assert response.status_code == 200
+    assert response.json() == []
+
+
+def test_get_reductions_for_instrument_reductions_exist():
+    response = client.get("/instrument/MARI/reductions?limit=3")
+    assert response.status_code == 200
+    assert response.json() == [
+        {
+            "id": 7107,
+            "reduction_end": None,
+            "reduction_inputs": {
+                "candidate": "RopSkhiHRdTOkfKlDTxS",
+                "data": 1068,
+                "foot": "xlypwBpsPGhzvRYMjclo",
+                "improve": False,
+                "in": "kxrFULFuYAGlwjjLwEXK",
+                "official": 2194,
+                "per": "DBXYUThgatRmqOwzOhge",
+                "sound": 19863726282.8752,
+                "up": True,
+                "whatever": "afXkrONWBczHRtfkSHBy",
+            },
+            "reduction_outputs": None,
+            "reduction_start": None,
+            "reduction_state": "NOT_STARTED",
+            "reduction_status_message": None,
+            "script": {"value": "import os\nprint('foo')\n"},
+        },
+        {
+            "id": 9882,
+            "reduction_end": "2019-02-27T07:17:09",
+            "reduction_inputs": {"growth": 4986, "make": -94539028.1069814},
+            "reduction_outputs": "What should this be?",
+            "reduction_start": "2019-02-27T07:02:09",
+            "reduction_state": "ERROR",
+            "reduction_status_message": "Speech clear education onto stay throughout face huge class.",
+            "script": {"value": "import os\nprint('foo')\n"},
+        },
+        {
+            "id": 7272,
+            "reduction_end": "2019-11-28T23:58:47",
+            "reduction_inputs": {
+                "her": 3159459467009.81,
+                "level": 7122,
+                "loss": "eHKbuJDBTlKMMYyYomdw",
+                "natural": -49.4505509648397,
+                "trade": 6447883.13642678,
+                "white": "KUtEOLGYqqAEJLRXSWWi",
+            },
+            "reduction_outputs": "What should this be?",
+            "reduction_start": "2019-11-28T23:14:47",
+            "reduction_state": "SUCCESSFUL",
+            "reduction_status_message": "Or not option history rate manager cover time own artist various ask.",
+            "script": {"value": "import os\nprint('foo')\n"},
+        },
+    ]
+
+
+def test_reductions_by_instrument_no_reductions():
+    """
+    Test empty array returned when no reductions for instrument
+    :return:
+    """
+    response = client.get("/instrument/test/reductions?limit=3")
+    assert response.status_code == 200
+    assert response.json() == []
