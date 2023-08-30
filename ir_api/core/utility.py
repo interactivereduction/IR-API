15813090e3ef7f6a29873a0e1e56c5100b10656a
Keiran Price <keiran.price@stfc.ac.uk>
2023-08-30 14:27:21 +0100

    Move disable

--- ir_api/core/utility.py
+++ ir_api/core/utility.py
@@ -10,7 +10,10 @@ def forbid_path_characters(func: Callable[[str], Any]) -> Callable[[str], Any]:
 
     @functools.wraps(func)
     def wrapper(arg: str):
-        if any(char in arg for char in (".", "/", "\\")):  # pylint: disable=anomalous-backslash-in-string
+        # pylint: disable=anomalous-backslash-in-string
+        if any(
+            char in arg for char in (".", "/", "\\")
+        ):
             raise UnsafePathError(f"Potentially unsafe path was requested: {arg}")
         return func(arg)
 
