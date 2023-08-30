f75c5148b1f3aa40fe7a71337cf41cced4b8fff7
Keiran Price <keiran.price@stfc.ac.uk>
2023-08-30 14:30:18 +0100

    Remove disable due to actually reading the pylint output

--- ir_api/core/utility.py
+++ ir_api/core/utility.py
@@ -6,14 +6,11 @@ from ir_api.core.exceptions import UnsafePathError
 
 
 def forbid_path_characters(func: Callable[[str], Any]) -> Callable[[str], Any]:
-    """Decorator that prevents path characters {/, ., \} from a functions args by raising UnsafePathError"""
+    """Decorator that prevents path characters {/, ., \\} from a functions args by raising UnsafePathError"""
 
     @functools.wraps(func)
     def wrapper(arg: str):
-        # pylint: disable=anomalous-backslash-in-string
-        if any(
-            char in arg for char in (".", "/", "\\")
-        ):
+        if any(char in arg for char in (".", "/", "\\")):
             raise UnsafePathError(f"Potentially unsafe path was requested: {arg}")
         return func(arg)
 
