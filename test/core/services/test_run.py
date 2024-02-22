fbc0e0c875e8196bd82bd69b4cc2e896ae271985
Keiran Price <keiran.price@stfc.ac.uk>
2024-02-22 08:49:45 +0000

    Update run service tests

--- test/core/services/test_run.py
+++ test/core/services/test_run.py
@@ -1,42 +1,43 @@
 """
 Tests for run service
 """
-from typing import Callable
 from unittest.mock import patch
 
 from ir_api.core.services.run import get_runs_by_instrument, get_run_count_by_instrument, get_total_run_count
 
 
-@patch("ir_api.core.services.run._RUN_REPO")
-def test_get_runs_by_instrument(mock_run_repo):
+@patch("ir_api.core.services.run._REPO")
+@patch("ir_api.core.services.run.RunSpecification")
+def test_get_runs_by_instrument(mock_spec_class, mock_run_repo):
     """
     Test that get_runs by instrument makes correct repo call
     :param mock_run_repo: Mock repo
     :return: None
     """
+    spec = mock_spec_class.return_value
     get_runs_by_instrument("test", limit=5, offset=6)
-    assert mock_run_repo.find.call_count == 1
-    assert mock_run_repo.find.call_args[1]["limit"] == 5
-    assert mock_run_repo.find.call_args[1]["offset"] == 6
+    mock_run_repo.find.assert_called_once_with(spec.by_instrument(instrument="test", limit=5, offset=6))
 
 
-@patch("ir_api.core.services.run._RUN_REPO")
-def test_get_run_count_by_instrument(mock_repo):
+@patch("ir_api.core.services.run._REPO")
+@patch("ir_api.core.services.run.RunSpecification")
+def test_get_run_count_by_instrument(mock_spec_class, mock_repo):
     """
     Test correct repo calls for count by instrument
     :return: None
     """
+    spec = mock_spec_class.return_value
     get_run_count_by_instrument("test")
-    assert mock_repo.count.call_count == 1
-    args, _ = mock_repo.count.call_args
-    assert isinstance(args[0], Callable)
+    mock_repo.count.assert_called_once_with(spec.by_instrument(instrument="test", limit=5, offset=6))
 
 
-@patch("ir_api.core.services.run._RUN_REPO")
-def test_get_total_run_count(mock_repo):
+@patch("ir_api.core.services.run._REPO")
+@patch("ir_api.core.services.run.RunSpecification")
+def test_get_total_run_count(mock_spec_class, mock_repo):
     """
     Test correct repo calls for counting all runs
     :return: None
     """
+    spec = mock_spec_class.return_value
     get_total_run_count()
-    mock_repo.count.assert_called_once()
+    mock_repo.count.assert_called_once_with(spec.all())
